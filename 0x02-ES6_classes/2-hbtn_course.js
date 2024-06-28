310c60d50bea428869769cad7ca8d516895b8942
emmydapson <iloriemmanuel00@gmail.com>
2024-06-28 01:15:28 +0000

    ES6_classes;classes, constructors,setters, getters etc

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,55 @@
+class HolbertonCourse {
+    constructor(name, length, students) {
+        this._name = this._validateString(name, 'Name');
+        this._length = this._validateNumber(length, 'Length');
+        this._students = this._validateArray(students, 'Students');
+    }
+
+    _validateString(value, attributeName) {
+        if (typeof value !== 'string') {
+            throw new TypeError(`${attributeName} must be a string`);
+        }
+        return value;
+    }
+
+    _validateNumber(value, attributeName) {
+        if (typeof value !== 'number') {
+            throw new TypeError(`${attributeName} must be a number`);
+        }
+        return value;
+    }
+
+    _validateArray(value, attributeName) {
+        if (!Array.isArray(value) || !value.every(item => typeof item === 'string')) {
+            throw new TypeError(`${attributeName} must be an array of strings`);
+        }
+        return value;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        this._name = this._validateString(value, 'Name');
+    }
+
+    get length() {
+        return this._length;
+    }
+
+    set length(value) {
+        this._length = this._validateNumber(value, 'Length');
+    }
+
+    get students() {
+        return this._students;
+    }
+
+    set students(value) {
+        this._students = this._validateArray(value, 'Students');
+    }
+}
+
+export default HolbertonCourse;
+
